47b1fba0fa075475fae2ee12a558dec07b82fff7
Zeevik Neuman <zneuman@myhippo.com>
2022-12-06 14:07:05 +0200

    api/states retrieve data using DB query

--- pages/api/states.ts
+++ pages/api/states.ts
@@ -1,14 +1,32 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import {GetStatesDataType} from "../../interfaces/api";
-import statesDataFile from '../../data/states_data'
+const { PG_PASSWORD } = process.env;
 
-const statesData: GetStatesDataType[] = statesDataFile
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<GetStatesDataType[]>
 ) {
+  const Pool = require("pg").Pool;
+  const config = {
+    host: "pod-staging.clv5lxmu2lpv.us-west-2.rds.amazonaws.com",
+    user: "awsuser",
+    password: PG_PASSWORD,
+    database: "staging"
+  };
+  const pool = new Pool(config);
+  const template = 'SELECT json_agg(t) FROM (\n' +
+      'SELECT STATE, \n' +
+      'COUNT(id) filter (WHERE product=\'ho3\') AS ho3,\n' +
+      'COUNT(id) filter (WHERE product=\'ho6\') AS ho6,\n' +
+      'COUNT(id) filter (WHERE product=\'dp3\') AS dp3,\n' +
+      'COUNT(id) filter (WHERE product=\'ho5\') AS ho5,\n' +
+      'COUNT(id)  AS total_in_state\n' +
+      'FROM policies WHERE status=\'active\'  AND "createdAt" > (CURRENT_DATE - INTERVAL \'30 DAY\')::DATE GROUP BY STATE' +
+      ') t'
+  const queryRes = await pool.query(template)
+  const statesData: GetStatesDataType[] = queryRes.rows[0].json_agg
+
   const allCount = statesData.reduce((a, c) => a + c.total_in_state, 0)
   statesData.forEach((s, dx) => statesData[dx].pct = statesData[dx].total_in_state / allCount);
   res.status(200).json(statesData);
